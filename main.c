47cf8e5901d007e550da6a8c90bfad2f9fe485f9
TylerJSurroca <tylerjsurroca@gmail.com>
2019-09-06 22:26:42 -0400

    Fixed code, added debugger

--- main.c
+++ main.c
@@ -5,35 +5,22 @@
 
 #define MAX_NAMELEN 32
 #define MAX_PEOPLE 32
+#define DEBUG 0
 
+/////////
+//Defines a person with a name, age, and location
+/////////
 struct person{
     char name[MAX_NAMELEN];
     int age;
     char location[MAX_NAMELEN];
 };
-/*
-int writeToFile(char name[MAX_NAMELEN])
-{
-    FILE *fptr;
 
-    fptr = fopen("C:\\Users\\Tyler\\Desktop\\IO name\\PeopleStorage.txt","w");
-
-    if (fptr == NULL)
-    {
-        printf("Error!");
-        exit(1);
-    }
-
-    struct person input1 = {"Tyler",19,"Longwood"};
-    struct person input2 = {"Francisco",49,"Longwood"};
-
-    fwrite (&input1, sizeof(struct person),1,fptr);
-    fwrite (&input2, sizeof(struct person),1,fptr);
-
-    fclose(fptr);
-}
-*/
-
+////////
+//Takes char filename and
+//Opens file for writing
+//Returns file pointer
+////////
 FILE * openFileForWrite(char filename[])
 {
     FILE *fptr;
@@ -50,13 +37,18 @@ FILE * openFileForWrite(char filename[])
     return fptr;
 }
 
-FILE * openFileForRead(char filename[])
+////////
+//Takes char filename and
+//Opens file for reading
+//Returns file pointer
+////////
+FILE* openFileForRead(char filename[])
 {
-    FILE *fptr;
+    FILE* fptr;
 
-    fptr = fopen(filename,"r");
+    fptr = fopen(filename, "r");
 
-    printf("File starts too open.\n");
+    if(DEBUG == 1)printf("File starts too open.\n");
 
     if (fptr == NULL)
     {
@@ -68,44 +60,31 @@ FILE * openFileForRead(char filename[])
     return fptr;
 }
 
-
+////////
+//Takes file pointer and closes file
+////////
 int closeFile(FILE *fptr)
 {
     fclose(fptr);
     return 0;
 }
 
+////////
+//Takes a file pointer and a personType structure
+//Writes the size of a person when called
+////////
 int writeToFile(FILE *fptr, struct person personType)
 {
     fwrite(&personType, sizeof(struct person), 1 , fptr);
     return 0;
 }
 
-/*
-int readFromFile(char name[MAX_NAMELEN])
+////////
+//Takes a file pointer and a personType structure
+//Reads through an array of people
+////////
+int readFromFile(FILE* fptr, struct person personArray[])
 {
-    FILE *fptr;
-
-    fptr = fopen("C:\\Users\\Tyler\\Desktop\\IO name\\PeopleStorage.txt","r");
-
-    if (fptr == NULL)
-    {
-        printf("ERROR could not open file!");
-        exit(1);
-    }
-
-    fscanf(fptr,"%s",name);
-
-    return 0;
-
-}
-*/
-
-//personArray must be initialized before passing in
-int readFromFile(struct person personArray[])
-{
-    FILE *fptr;
-
     int i = 0;
 
     while(fread(&personArray[i], sizeof(struct person),1,fptr))
@@ -113,30 +92,16 @@ int readFromFile(struct person personArray[])
         i++;
     }
 
-    fclose(fptr);
-
     return i;
 }
 
 int main()
 {
-    /*
-    char name[MAX_NAMELEN];
-
-    printf("What is your name?\n");
-    scanf("%s",name);
-
-    writeToFile(name);
-
-    readFromFile(name);
-    printf("Your name is: %s",name);
-    */
-
     char name[MAX_NAMELEN];
     int age;
     char location[MAX_NAMELEN];
     char filename[] = "C:\\Users\\Tyler\\Desktop\\IO name\\PeopleStorage.bin";
-    char cont = 'y';
+    char cont[] = "y";
 
     struct person personType;
 
@@ -144,7 +109,7 @@ int main()
 
     fptr = openFileForWrite(filename);
 
-    while(cont == 'y')
+    while(cont[0] == 'y')
     {
         printf("Enter your name: \n");
         scanf("%s",&name);
@@ -162,45 +127,29 @@ int main()
         printf("Do you want to continue? \n");
         scanf("%s",&cont);
     }
-    printf("\nIf this prints the while loop is broken\n");
+    if(DEBUG == 1)printf("If this prints the while loop is broken\n");
 
     closeFile(fptr);
 
-    printf("\nFile successfully closes\n");
-
-    //Begin reading file
-    //fptr = openFileForRead(filename);
-    fptr = fopen(filename,"r");
-
-    printf("\nFile successfully opens!");
+    if(DEBUG == 1)printf("File successfully closes\n");
+    if(DEBUG == 1)printf("File successfully opens!");
 
     struct person readPeople[MAX_PEOPLE];
     int count;
 
-    printf("\n\nFile opened correctly");
+    fptr = openFileForRead(filename);
 
-    readFromFile(readPeople);
+    if(DEBUG == 1)printf("File opened correctly\n");
 
-    printf("The count is: %d",count);
+    count = readFromFile(fptr, readPeople);
+    closeFile(fptr);
+
+    if(DEBUG == 1)printf("The count is: %d",count);
 
     for(int i = 0; i < count; i++)
     {
         printf("\nName: %s.\nAge: %d.\nLocation: %s.\n\n", readPeople[i].name, readPeople[i].age, readPeople[i].location);
     }
-
-    closeFile(fptr);
-
-
-    /*
-    struct person input1 = {"Tyler" , 19 , "Longwood"};
-    struct person input2 = {"Francisco" , 49 , "Longwood"};
-    struct person input3 = {"Trevor" , 25 , "Lakeland"};
-
-    fwrite (&input1, sizeof(struct person),1,fptr);
-    fwrite (&input2, sizeof(struct person),1,fptr);
-    fwrite (&input3, sizeof(struct person),1,fptr);
-    */
-
 
     return 0;
 }
